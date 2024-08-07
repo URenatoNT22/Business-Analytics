f4da2020b29e53b229d4a0446883703560253f5e
Jogaqui <96966786+Jogaqui@users.noreply.github.com>
2024-08-06 23:39:07 -0500

    subiendo repositorio guía

---
+++ repositorio-guia/services/database.py
@@ -0,0 +1,17 @@
+import mysql.connector
+from os import environ
+
+HOST = environ.get('HOST', 'localhost')
+PASSWD = environ.get('PASSWD', '')
+USER = environ.get('USER', 'root')
+DATABASE = environ.get('DATABASE', 'python-crud-streamlit')
+
+bdConnection = mysql.connector.connect(
+    host=f'{HOST}',
+    user=f'{USER}',
+    password=f'{PASSWD}',
+    database=f'{DATABASE}',
+)  # Conexão com dados do banco de dados e MySQL
+
+cursor = bdConnection.cursor()  # Cursor para executar conexão
+bdConnection.commit()
