2d9e48e029c9cd392a53d74f0a6fd35ce16e6ceb
Jogaqui <96966786+Jogaqui@users.noreply.github.com>
2024-08-09 00:50:19 -0500

    agregar configuración de .env

--- database.py
+++ database.py
@@ -1,11 +1,22 @@
+import os
+from dotenv import load_dotenv
 import mysql.connector
+
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
+
+# Obtener los valores de las variables de entorno
+host = os.getenv("DB_HOST")
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+database = os.getenv("DB_NAME")
 
 # Establecer la conexión a la base de datos MySQL
 conn = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="12345678",
-    database="heartDiseaseApp"
+    host=host,
+    user=user,
+    password=password,
+    database=database
 )
 cursor = conn.cursor()
 
